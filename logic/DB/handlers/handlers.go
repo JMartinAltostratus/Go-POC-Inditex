273030f6c29285d8297fc66bbcf816ee1c4a986d
JMGAltostratus <j.martingarcia@altostratus.es>
2022-03-03 15:24:29 +0100

    Update handlers.go

--- logic/DB/handlers/handlers.go
+++ logic/DB/handlers/handlers.go
@@ -2,16 +2,33 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/JMartinAltostratus/Go-POC-Inditex/logic/models"
 	"github.com/gin-gonic/gin"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+	"io"
+	"log"
 	"net/http"
+	"reflect"
 )
 
-type createRequest struct {
+//Este es el modelo de datos de la request que me va a LLEGAR,
+//habría que factorizarlo para el objeto tipo NOTA (models note) que voy a guardar
+//en la DB, para así poder hacer un parse y convertir las relaciones
+//en lo que tiene que ser y etc etc
+type request struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 	Text string `json:"content"`
 	//Relationships como un array de objetos nota??
 }
+
+const (
+	dbUser = "neo4j"
+	dbPass = "posts-preposition-heats"
+	dbHost = "bolt://3.239.185.213"
+	dbPort = ":7687"
+	dbName = "Blank Sandbox"
+)
 
 // SearchElastic Hacer conexion con Elastic y buscar en funcion de una palabra o whatever
 func SearchElastic() gin.HandlerFunc {
@@ -21,7 +38,7 @@ func SearchElastic() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		//Hacer cosas en funcion del archivo en el que esté
 		fmt.Printf("CreateHandler correcto \n")    //LLEGA.
-		var req createRequest                      //Me declaro una request con la forma del struct de arriba
+		var req request                            //Me declaro una request con la forma del struct de arriba
 		if err := ctx.BindJSON(&req); err != nil { //Aquí se usa gin para gestionar la petición y modifico el objeto anterior
 			ctx.JSON(http.StatusBadRequest, err.Error()) //En caso de que no vaya, se devuelve un badrequest 400
 			return
@@ -33,15 +50,52 @@ func SearchElastic() gin.HandlerFunc {
 // a la función que haya que llamar, en este caso search
 func SearchNeo4J() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+		var req request
+		if err := ctx.BindJSON(&req); err != nil {
+			ctx.JSON(http.StatusBadRequest, err.Error())
+			return
+		}
 		ctx.String(http.StatusOK, "searchNeo4J ha funcionao")
 		//EJEMPLO, ESTA NOTA HABRÍA QUE GUARDARLA EN OTRO CONTEXTO
 		ctx.Status(http.StatusCreated) //Un 201 si va bien
 		//fmt.Print(note.ID())
 
+		// -------- Conexion con la neo4J
+		//nota := models.NewNote(req.ID, req.Name, req.Text, nil)
+		driver, err := neo4j.NewDriver(dbHost, neo4j.BasicAuth(dbUser, dbPass, ""))
+		defer func() { err = handleClose(driver, err) }()
+		session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite, DatabaseName: dbName})
+		defer func() { err = handleClose(session, err) }()
+		createNote(session, models.Note{})
+		//createRelation(session, models.Relation{})
 	}
 
-	//Hacer cosas
+}
+
+func createNote(session neo4j.Session, note models.Note) {
+	query := ""
+	if note.ID() == "" {
+		query += fmt.Sprintf(`CREATE (:Note {idNote: "%s", name: "%s",content: "%s"})})`, note.ID(), note.Name(), note.Content())
+	} else {
+		print("La nota ya existe")
+	}
+	//map[string]interface{}{}??????????????
+	r, err := session.Run(query, map[string]interface{}{})
+	fmt.Println(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleClose(closer io.Closer, previousError error) error {
+	err := closer.Close()
+	if err == nil {
+		return previousError
+	}
+	if previousError == nil {
+		return err
+	}
+	return fmt.Errorf("%v closure error occurred:\n%s\ninitial error was:\n%w", reflect.TypeOf(closer), err.Error(), previousError)
 }
 
 // UpdateNote Update a la BD Neo4J con los cambios en la nota
